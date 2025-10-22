6269542b67ce63f691eb00584b1f6b9eac47c140
estarking <134989706+estarking@users.noreply.github.com>
2025-10-22 17:31:19 +0800

    Comment out version_main in Chrome driver setup

--- nodeloc.py
+++ nodeloc.py
@@ -93,7 +93,7 @@ def setup_browser():
             options=options,
             #driver_executable_path='/usr/bin/chromedriver',
             driver_executable_path=None,
-            version_main=138,
+            version_main=None,
             use_subprocess=True
         )
         driver.set_window_size(1920, 1080)
