044b55672dc4393f916a1c1c40e27ab65d4ff90f
estarking <134989706+estarking@users.noreply.github.com>
2025-10-23 09:06:56 +0800

    Comment out version_main in Chrome driver setup

--- nodeloc.py
+++ nodeloc.py
@@ -81,7 +81,7 @@ def setup_browser():
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
     options.add_argument('--window-size=1920,1080')
-    options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0 Safari/537.36')
+    options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0 Safari/537.36')
     options.add_argument('--disable-blink-features=AutomationControlled')
     options.add_argument('--disable-extensions')
     options.add_argument('--disable-infobars')
